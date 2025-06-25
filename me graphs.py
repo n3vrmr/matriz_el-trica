f29121d120bd35ba30bff42f6d293b6bf5941c58
Nevermore <102398462+n3vrmr@users.noreply.github.com>
2025-06-25 00:40:33 -0300

    Add files via upload

--- me graphs.py
+++ me graphs.py
@@ -97,8 +97,8 @@ new_df.to_excel("Tabela 1.xlsx")
 
 ax = sns.barplot(final_1,x="Value",y="Area",hue="Year",palette="Spectral_r")
 ax.set_title(
-    "Participação de fontes de energia renovável na matriz elétrica dos países"
-    )
+    "Total de fontes de Energia Renovável na Matriz Elétrica dos países"
+    , fontsize=18, pad=15)
 ax.set_xlabel("Valor em %")
 ax.set_xlim(right=100)
 ax.set_ylabel("País")
@@ -117,10 +117,195 @@ espf_24_reduced = espf_2024_ptbr.drop(labels=
                                         "Unit","YoY absolute change"
                                         ], axis=1).reset_index(drop=True)
 
+# Os dados não incluem energia nuclear gerada pela Austrália, por algum motivo
+# Pra não dar merda depois, é melhor adicionar uma linha agora
+
+espf_24_reduced.loc[72.5] = ["Austrália",2024,"Nuclear",0,np.nan]
+espf_24_reduced = espf_24_reduced.sort_index().reset_index(drop=True)
+
+def ptbr_energysource(data):
+    
+    data.loc[data["Variable"] == "Bioenergy", "Variable"] = "Bioenergia"
+    data.loc[data["Variable"] == "Coal", "Variable"] = "Carvão"
+    data.loc[data["Variable"] == "Gas", "Variable"] = "Gás"
+    data.loc[data["Variable"] == "Hydro", "Variable"] = "Hidráulica"
+    data.loc[data["Variable"] == "Other Fossil", "Variable"] = "Outras fósseis"
+    data.loc[data["Variable"] == "Wind", "Variable"] = "Eólica"
+    data.loc[data["Variable"] == "Other Renewables",
+             "Variable"] = "Outras renováveis"
+    
+    return data
+
+espf24r_ptbr = ptbr_energysource(espf_24_reduced).reset_index(drop=True)
+
+def sum_sources(data):
+    
+    data.loc[data["Variable"] == "Bioenergia",
+             "Variable"] = "Outras renováveis"
+    data.loc[data["Variable"] == "Solar",
+             "Variable"] = "Solar e eólica"
+    data.loc[data["Variable"] == "Eólica",
+             "Variable"] = "Solar e eólica"
+    
+    data_sum = data.groupby(["Area","Variable"], as_index=False,
+                            sort=False)["Value"].sum()
+    
+    return data_sum
+
+summed_sources = sum_sources(espf24r_ptbr)
+
+paises = list(espf24r_ptbr["Area"].value_counts(sort=False).index)
+
+# There's probably a better way to do this...
+brasil = summed_sources[summed_sources["Area"] == "Brasil"]
+canada = summed_sources[summed_sources["Area"] == "Canadá"].reset_index(
+    drop=True)
+alemanha = summed_sources[summed_sources["Area"] == "Alemanha"].reset_index(
+    drop=True)
+espanha = summed_sources[summed_sources["Area"] == "Espanha"].reset_index(
+    drop=True)
+uk = summed_sources[summed_sources["Area"] == "Reino Unido"].reset_index(
+    drop=True)
+italia = summed_sources[summed_sources["Area"] == "Itália"].reset_index(
+    drop=True)
+ue = summed_sources[summed_sources[
+    "Area"] == "União Europeia"].reset_index(drop=True)
+turquia = summed_sources[summed_sources["Area"] == "Turquia"].reset_index(
+    drop=True)
+australia = summed_sources[summed_sources[
+    "Area"] == "Austrália"].reset_index(drop=True)
+argentina = summed_sources[summed_sources[
+    "Area"] == "Argentina"].reset_index(drop=True)
+china = summed_sources[summed_sources["Area"] == "China"].reset_index(
+    drop=True)
+japao = summed_sources[summed_sources["Area"] == "Japão"].reset_index(
+    drop=True)
+franca = summed_sources[summed_sources["Area"] == "França"].reset_index(
+    drop=True)
+eua = summed_sources[summed_sources["Area"] == "EUA"].reset_index(
+    drop=True)
+india = summed_sources[summed_sources["Area"] == "Índia"].reset_index(
+    drop=True)
+mexico = summed_sources[summed_sources["Area"] == "México"].reset_index(
+    drop=True)
+russia = summed_sources[summed_sources["Area"] == "Rússia"].reset_index(
+    drop=True)
+africadosul = summed_sources[summed_sources[
+    "Area"] == "África do Sul"].reset_index(drop=True)
+coreiadosul = summed_sources[summed_sources[
+    "Area"] == "Coreia do Sul"].reset_index(drop=True)
+
+category_names = ['Hidráulica', 'Solar e eólica', 'Outras renováveis',
+                  'Carvão','Gás','Outras fósseis','Nuclear']
+
+# Below is just copied from matplotlib's barh example
+
+values_percent = {
+    'Brasil': [float
+               (brasil[brasil["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Canadá': [float
+               (canada[canada["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Alemanha': [float
+               (alemanha[alemanha["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Espanha': [float
+               (espanha[espanha["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Reino Unido': [float
+               (uk[uk["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Itália': [float
+               (italia[italia["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'União Europeia': [float
+               (ue[ue["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Turquia': [float
+               (turquia[turquia["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Austrália': [float
+               (australia[australia["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Argentina': [float
+               (argentina[argentina["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'China': [float
+               (china[china["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Japão': [float
+               (japao[japao["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'França': [float
+               (franca[franca["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'EUA': [float
+               (eua[eua["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Índia': [float
+               (india[india["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'México': [float
+               (mexico[mexico["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Rússia': [float
+               (russia[russia["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'África do Sul': [float
+               (africadosul[africadosul["Variable"] == i].iloc[0,2]
+                ) for i in category_names],
+    'Coreia do Sul': [float
+               (coreiadosul[coreiadosul["Variable"] == i].iloc[0,2]
+                ) for i in category_names]}
+
+
+def graph(values_percent, category_names):
+    """
+    Parameters
+    ----------
+    values_percent : dict
+        A mapping from question labels to a list of answers per category.
+        It is assumed all lists contain the same number of entries and that
+        it matches the length of *category_names*.
+    category_names : list of str
+        The category labels.
+    """
+    labels = list(values_percent.keys())
+    data = np.array(list(values_percent.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = plt.colormaps['terrain'](
+        np.linspace(0.15, 0.85, data.shape[1]))
+
+    fig, ax = plt.subplots(figsize=(9.2, 5))
+    ax.set_title("Composição da Matriz Elétrica dos países", fontsize=18,
+                 pad=30)
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(True)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+    ax.set_xlabel("Participação da fonte de energia elétrica em %")
+
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        rects = ax.barh(labels, widths, left=starts, height=0.5,
+                        label=colname, color=color)
+
+        r, g, b, _ = color
+        text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        # ax.bar_label(rects, label_type='center', color=text_color)
+    ax.legend(ncols=len(category_names), bbox_to_anchor=(0, 1),
+              loc='lower left', fontsize='small')
+
+    return fig, ax
+
+
+graph(values_percent, category_names)
+plt.show()
 
 def main():
     if __name__ == '__main__':
-        print("Qual é a dificuldade de usar os dados certos, Poder360?")
+        print("Qual é a dificuldade de NÃO USAR IA PRA FAZER ISSO, Poder360?")
     return
 
 main()
